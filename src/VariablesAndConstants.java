9dfae36f3c6560ee0754bb2e54d21947bf9b045f
zandoli <vaughnaimable@gmail.com>
2016-11-11 18:23:51 -0400

    Ini

---
+++ src/VariablesAndConstants.java
@@ -0,0 +1,9 @@
+public class VariablesAndConstants {
+    public static void main(String[] args) {
+        final float salary = 236.35f;
+        System.out.println(salary);
+
+        char myCharacter = 'a';
+        System.out.println(myCharacter);
+    }
+}
