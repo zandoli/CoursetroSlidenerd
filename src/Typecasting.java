8e90699073029d9c3efce9c650d8901fd74cabb4
zandoli <vaughnaimable@gmail.com>
2016-11-12 12:17:09 -0400

    Iniitial

---
+++ src/Typecasting.java
@@ -0,0 +1,12 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        long age = 26147586932145L;
+        int yourAge = (int)age;
+        System.out.println(age);
+        System.out.println(yourAge);
+
+        int letter = 90;
+        char value = (char)letter;
+        System.out.println(value);
+    }
+}
