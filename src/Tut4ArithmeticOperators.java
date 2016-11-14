6578aaa4c48adbae6da7b17b9f31c5c3430f426c
zandoli <vaughnaimable@gmail.com>
2016-11-14 19:52:42 -0400

    Iniitial

---
+++ src/Tut4ArithmeticOperators.java
@@ -0,0 +1,21 @@
+public class Tut4ArithmeticOperators {
+    public static void main(String[] args) {
+        //what is the average of 1 + 2 + 3 + 4 + 5?
+        int average = (1 + 2 + 3 + 4 + 5)/5;
+        System.out.println(average);
+
+        double avg  = 9/2;
+        double avg1  = 9/2.0;
+        double avg2  = (double) 9/2;
+        System.out.println(avg);
+        System.out.println(avg1);
+        System.out.println(avg2);
+
+        int ans = 20 % 3;
+        System.out.println(ans);
+        int ans1 = 20 % 2;
+        System.out.println(ans1);
+
+
+    }
+}
