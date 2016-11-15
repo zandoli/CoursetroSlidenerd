790075f1e9b1dcf20baa9f5aad3d8e113eaa37c3
zandoli <vaughnaimable@gmail.com>
2016-11-14 23:05:22 -0400

    Iniitial

---
+++ src/Tut5RelationalOperators.java
@@ -0,0 +1,11 @@
+public class Tut5RelationalOperators {
+    public static void main(String[] args) {
+        boolean result = 5 > 4;
+        System.out.println(result);
+
+        int num1 = 5;
+        int num2 = 4;
+        boolean res = num2 > num1;
+        System.out.println(res);
+    }
+}
---
+++ src/Tut6RelationalOperators2.java
@@ -0,0 +1,24 @@
+public class Tut6RelationalOperators2 {
+    public static void main(String[] args) {
+        int myAge = 26;
+        int yourAge = 18;
+
+        boolean res = myAge == yourAge;
+        //System.out.println(res);
+
+        //if (res == false){
+            //System.out.println(myAge);
+        //}
+
+        double num1 = 0.7 + 0.1;
+        double num2 = 0.9 - 0.1;
+
+        int res1 = Double.compare(num2,num1);
+
+        System.out.println(num1);
+        System.out.println(num2);
+        //System.out.println(num1==num2);
+        System.out.println(res1);
+
+    }
+}
