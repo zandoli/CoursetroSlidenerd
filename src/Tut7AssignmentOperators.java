2572a8d513c5c2498952d8d14294ab7fd225add3
zandoli <vaughnaimable@gmail.com>
2016-11-15 11:59:27 -0400

    Iniitial

---
+++ src/Tut7AssignmentOperators.java
@@ -0,0 +1,25 @@
+public class Tut7AssignmentOperators {
+    public static void main(String[] args) {
+        int num1=5, num2=4;
+        System.out.println(num1);
+
+        //num1 = num1 + num2;
+        //System.out.println(num1);
+
+        //num1 += num2;
+        //System.out.println(num1);
+
+        //num1 -= num2;
+        //System.out.println(num1);
+
+        //num1 *= num2;
+        //System.out.println(num1);
+
+        //num1 /= num2;
+        //System.out.println(num1);
+
+        num1 %= num2;
+        System.out.println(num1);
+
+    }
+}
---
+++ src/Tut8AssignmentOperators2.java
@@ -0,0 +1,21 @@
+public class Tut8AssignmentOperators2 {
+    public static void main(String[] args) {
+        int age = 26, newAge = 4;
+
+        //newAge = newAge + age;
+        //System.out.println(newAge);
+
+        //newAge += age;
+        //System.out.println(newAge);
+
+        //newAge -= age;
+        //System.out.println(newAge);
+
+        age /= newAge;
+        System.out.println(age);
+
+        age -= newAge;
+        System.out.println(age);
+
+    }
+}
