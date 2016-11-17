59eaa28737cb903a81d23d831c985b90d77043c6
zandoli <vaughnaimable@gmail.com>
2016-11-16 22:38:27 -0400

    Iniitial

---
+++ src/Tut12TernaryOperator.java
@@ -0,0 +1,7 @@
+public class Tut12TernaryOperator {
+    public static void main(String[] args) {
+        int marks=91;
+        char grade = marks>=90?'A':'B';
+        System.out.println(grade);
+    }
+}
---
+++ src/Tut13TerneryOperator2.java
@@ -0,0 +1,15 @@
+public class Tut13TerneryOperator2 {
+    public static void main(String[] args) {
+        int marks=2;
+        char grade = marks>75?'P':'F';
+
+        //*char grade;
+        //if(marks>90){
+        //    grade='A';
+        //}
+        //else{
+        //    grade='B';
+        //}
+        System.out.println(grade);
+    }
+}
