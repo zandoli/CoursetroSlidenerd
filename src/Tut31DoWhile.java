60c171743e4423c962f0e7e86ec7ef88baf35887
zandoli <vaughnaimable@gmail.com>
2016-11-26 18:41:50 -0400

    Iniitial

---
+++ src/Tut31DoWhile.java
@@ -0,0 +1,14 @@
+public class Tut31DoWhile {
+    public static void main(String[] args) {
+        int a = 0;
+
+        do{
+            System.out.println("run");
+            a++;
+        }while(a<4);
+
+    }
+}
+
+
+
