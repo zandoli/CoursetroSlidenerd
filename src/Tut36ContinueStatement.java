9a683469e783232aa782db208970776fe921e6a4
zandoli <vaughnaimable@gmail.com>
2016-12-05 02:31:31 -0400

    Iniitial

---
+++ src/Tut36ContinueStatement.java
@@ -0,0 +1,11 @@
+public class Tut36ContinueStatement {
+    public static void main(String[] args) {
+        for (int i = -10; i < 10; i++) {
+                if(i==0){
+                    continue;
+                }
+            System.out.println(100/i);
+
+        }
+    }
+}
