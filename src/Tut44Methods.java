3f9080f63ec0a641226d0e4604457c78718b19ef
zandoli <vaughnaimable@gmail.com>
2016-12-17 21:55:57 -0400

    Iniitial

---
+++ src/Tut44Methods.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Tut44Methods {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is the divisor?");
+        //String sci = sc.nextLine();
+        int divisor = Integer.parseInt(sc.nextLine());
+
+        //int number = 175;
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("What is the dividend?");
+        //String sci1 = sc.nextLine();
+        int dividend = Integer.parseInt(sc1.nextLine());
+
+        //boolean result =isDivisibleBySeven(number);
+        //System.out.println(result);
+        System.out.println(isDivisibleBy(divisor, dividend));
+    }
+
+    private static boolean isDivisibleBy(int no, int div ){
+        return no % div == 0;
+
+    }
+}
