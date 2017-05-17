74b3e0f6b7ee67dc336029f4f03ed2daad94d9d8
zandoli <vaughnaimable@gmail.com>
2017-05-17 15:21:45 -0400

    Initial

--- src/test.java
+++ src/test.java
@@ -5,12 +5,19 @@ public class test {
         //numbers[5] = 8; //methodA
 
         //int[] scores= {10,9,8,6,7}; //methodB
-        double sum=0;
 
         int[] numbers = {34,25,56,77,65,17,48,81};
+        double sum=0;
         for (int number : numbers) {
             sum += number;
-            System.out.println(number);
+            //System.out.println(number);
+        /*int[] numbers1 = {34,25,56,77,65,17,48,81};
+        double sum1=0;
+
+        for(int x=0; x<numbers1.length;x++){
+            sum1 += numbers1[x];*/
         }System.out.println(sum);
+        //System.out.println(sum1);
     }
+
 }
