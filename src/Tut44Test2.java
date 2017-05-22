9da51275e774da762f05e16cf1313ac22e2560e8
zandoli <vaughnaimable@gmail.com>
2017-05-22 16:23:10 -0400

    Initial

--- src/Tut44Test2.java
+++ src/Tut44Test2.java
@@ -7,17 +7,13 @@ public class Tut44Test2 {
     System.out.println("What is the length of the cube in cm?");
 
     Integer input = Integer.parseInt(in.nextLine());
-
         System.out.println("Area of 1 of the cube's sides is "+square(input)+" cm^2");
-
     System.out.println("Volume of the cube is: "+volumeOfCube(input)+" cm^3");
 }
     private static double square(double length){
-
             return length*length;
             }
     private static double volumeOfCube(double length){
-
         return length*length*length;
         }
 }
