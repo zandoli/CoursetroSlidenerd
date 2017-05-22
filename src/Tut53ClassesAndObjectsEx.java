06fbae609249de1b9d6359137f5624b9bae9c561
zandoli <vaughnaimable@gmail.com>
2017-05-22 15:41:52 -0400

    Initial

--- src/Tut53ClassesAndObjectsEx.java
+++ src/Tut53ClassesAndObjectsEx.java
@@ -1,14 +1,14 @@
 public class Tut53ClassesAndObjectsEx {
     public static void main(String[] args) {
-//        Square sq = null;
-//        sq = new Square();
-//        sq.side = 3;
-//
-//        Square sq2 = new Square();
-//        sq2.side = 9;
+/*      Square sq = null;
+        sq = new Square();
+        sq.side = 3;
 
-//        System.out.println(sq.calculateArea());
-//        System.out.println(sq2.calculateArea());
+        Square sq2 = new Square();
+        sq2.side = 9;
+
+        System.out.println(sq.calculateArea());
+        System.out.println(sq2.calculateArea());*/
 
         for (int i = 1; i <= 10 ; i++) {
             Square sq = new Square();
@@ -16,7 +16,6 @@ public class Tut53ClassesAndObjectsEx {
 
             System.out.println(sq.calculateArea());
         }
-
         for (int i = 1; i <= 10 ; i++) {
             Rectangle rec = new Rectangle();
             rec.side1 = i;
@@ -24,30 +23,32 @@ public class Tut53ClassesAndObjectsEx {
 
             System.out.println(rec.calculateArea1());
         }
+        for (int i = 1; i <= 10 ; i++) {
+            Triangle tr = new Triangle();
+            tr.side3 = i;
+            tr.side4 = 5;
+
+            System.out.println(tr.calculateArea2());
+        }
     }
 }
-
 class Square{
     double side;
-
     double calculateArea(){
         return side*side;
     }
 }
-
 class Rectangle{
     double side1;
     double side2;
-
-
-
     double calculateArea1(){
         return side1*side2;
     }
-
-
 }
-
 class Triangle{
-
+    double side3;
+    double side4;
+    double calculateArea2(){
+        return 0.5*side3*side4;
+    }
 }
